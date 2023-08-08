fb9d5ec3a9d7846dc7dd0b8db5b296dd1a6616bb
Paflias <paflias13@gmail.com>
2023-08-08 16:06:52 +0300

    Ex.03 done.

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,13 +1,15 @@
+import { InputBase$ChangeEvent } from "sap/m/InputBase";
 import BaseController from "./BaseController";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import Input from "sap/m/Input";
 
 type WeatherInfo = {
 	current_weather: {
-		temperature: number,
-		windspeed: number,
-		winddirection: number
-	}
-}
+		temperature: number;
+		windspeed: number;
+		winddirection: number;
+	};
+};
 
 /**
  * @namespace com.myorg.myapp.controller
@@ -17,6 +19,15 @@ export default class Main extends BaseController {
 		const model = new JSONModel();
 		this.setModel(model);
 		void this.loadWeatherData();
+
+		const input = this.byId("location");
+		if (input.isA<Input>("sap.m.Input")) {
+			// type guard (unfortunately the control class needs to be given twice)
+			input.attachChange(function (evt) {
+				// now TS knows input is an Input
+				const location = evt.getParameter("value"); // type safety even for string-based access
+			});
+		}
 	}
 
 	async loadWeatherData(lat = "49.31", lon = "8.64") {
@@ -24,7 +35,11 @@ export default class Main extends BaseController {
 		const response = await fetch(
 			`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current_weather=true`
 		);
-		const jsonData = await response.json() as WeatherInfo;
+		const jsonData = (await response.json()) as WeatherInfo;
 		(this.getModel() as JSONModel).setData(jsonData);
+	}
+
+	locationChange(evt: InputBase$ChangeEvent) {
+		const location = evt.getParameters().value;
 	}
 }
